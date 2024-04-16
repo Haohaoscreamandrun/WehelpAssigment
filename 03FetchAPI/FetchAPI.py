19c1d33432675f5d8212cce553d1afdacd0ac6bd
J8426 <J842671395@gmail.com>
2024-04-16 09:45:36 +0800

    variables name update

--- 03FetchAPI/FetchAPI.py
+++ 03FetchAPI/FetchAPI.py
@@ -1,10 +1,10 @@
 # write/open best solution
 import json
 with open("03FetchAPI/taipei-attractions-assignment-1.json", mode = "r", encoding= 'utf-8') as file:
-    data1 = json.load(file)
+    taipeiAttractions = json.load(file)
 
 with open("03FetchAPI/taipei-attractions-assignment-2.json", mode="r", encoding='utf-8') as file:
-    data2 = json.load(file)
+    stationMRT = json.load(file)
 
 
 ## 'stitle' is the Spot title
@@ -20,15 +20,16 @@ import csv
 tableSpot = [['SpotTitle', 'District', 'Longitude', 'Latitude', 'ImageURL']]
 
 # extract data from source json
-for spot in data1['data']['results']:
+for spot in taipeiAttractions['data']['results']:
     spotTitle = spot['stitle']
-    for mrt in data2['data']:
+    for mrt in stationMRT['data']:
         if mrt['SERIAL_NO'] == spot['SERIAL_NO']:
-            district = mrt['address'][5:8]
-    longitude = spot['longitude']
-    latitude = spot['latitude']
-    imgURL = 'https' + spot['filelist'].split("https", 2)[1]
-    tableSpot.append([spotTitle,district,longitude,latitude,imgURL])
+            spotDistrict = mrt['address'][5:8]
+    spotLongitude = spot['longitude']
+    spotLatitude = spot['latitude']
+    spotImgURL = 'https' + spot['filelist'].split("https", 2)[1]
+    tableSpot.append(
+        [spotTitle, spotDistrict, spotLongitude, spotLatitude, spotImgURL])
 
 # write into csv
 # 'utf-8' is not working - The \ufeff is a Byte Order Mark that can often be found on Windows UTF-8 files, and it might be confusing csv. Try using utf-8-sig for the encoding.
