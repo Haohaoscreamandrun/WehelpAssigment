618450ee180a6206ef293afc042c17ee55834b8d
J8426 <J842671395@gmail.com>
2024-04-25 22:33:51 +0800

    Week4: task3 completed

--- 04FastAPI/main.py
+++ 04FastAPI/main.py
@@ -2,14 +2,23 @@ from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key="APP_AUTHORIZED", max_age= 3600)
+
 app.mount("/static", StaticFiles(directory="static"), name= 'static')
 
 templates = Jinja2Templates(directory= 'templates')
 
+
 @app.get("/", response_class= HTMLResponse)
 async def read_item(request: Request):
+  if request.session["SIGNED-IN"] == False or len(request.session.keys()) == 0:
+    request.session.update({"SIGNED-IN": False})
   return templates.TemplateResponse(
     request = request, name = 'login.html'
   )
@@ -17,8 +26,9 @@ async def read_item(request: Request):
 # Use the templates you created to render and return a TemplateResponse, pass the name of the template, the request object, and a "context" dictionary with key-value pairs to be used inside of the Jinja2 template.
 # By declaring response_class=HTMLResponse the docs UI will be able to know that the response will be HTML.
 
-@app.post("/signin/")
-async def login(account: str= Form(None), pwd: str= Form(None)):
+
+@app.post("/signin/", response_class=HTMLResponse)
+async def login(request: Request, account: str= Form(None), pwd: str= Form(None)):
   if account is None or pwd is None or account.strip() == "" or pwd.strip() == "":
     response = RedirectResponse(url="/error/?message=請輸入帳號與密碼", status_code=302)
     return response
@@ -26,6 +36,8 @@ async def login(account: str= Form(None), pwd: str= Form(None)):
     response = RedirectResponse(url="/error/?message=帳號或密碼輸入錯誤", status_code=302)
     return response
   elif account == "test" and pwd == "test":
+    # Upon successful login, set a flag in the session to indicate SIGNED-IN
+    request.session.update({"SIGNED-IN": True})
     response = RedirectResponse(url="/member/", status_code=302) #302 is necessary for redirecting
     return response
 
@@ -41,7 +53,17 @@ async def error(request: Request, message: str):
 
 @app.get("/member/", response_class=HTMLResponse)
 async def success(request: Request):
-  return templates.TemplateResponse(
+  if not request.session["SIGNED-IN"] == True:
+    response = RedirectResponse(url= "/", status_code= 302)
+    return response
+  else:
+    return templates.TemplateResponse(
     request= request,
     name='member.html',
   )
+
+@app.get("/signout/", response_class=HTMLResponse)
+async def logout(request: Request):
+  request.session.update({"SIGNED-IN": False})
+  response = RedirectResponse(url="/", status_code=302)
+  return response
