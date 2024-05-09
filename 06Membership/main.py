85ff00fff30248c302914bdcb8c9fcbb2a368fe5
“ChenHao” <j842671395@gmail.com>
2024-05-09 12:57:40 +0800

    Week6: Task3-6 completed

--- 06Membership/main.py
+++ 06Membership/main.py
@@ -59,7 +59,7 @@ async def signup(request: Request, username: str = Form(None), account: str = Fo
         mycursor.execute(sql, val)
         mydb.commit()
         #redirect to homepage with query message
-        return RedirectResponse(url="/?message=註冊成功", status_code=303)
+        return RedirectResponse(url="/?message=註冊成功，請由下方登入", status_code=303)
         
 
 @app.post("/signin", response_class=HTMLResponse)
@@ -74,7 +74,7 @@ async def login(request: Request, account: str = Form(None), password: str = For
     )
     mycursor = mydb.cursor()
     # select all account and password
-    sql ='SELECT account, password FROM member WHERE account = %s AND password = %s'
+    sql ='SELECT * FROM member WHERE account = %s AND password = %s'
     query = (f'{account}', f'{password}',)
     mycursor.execute(sql,query)
     myresult = mycursor.fetchall()
@@ -83,6 +83,15 @@ async def login(request: Request, account: str = Form(None), password: str = For
     else:
         for row in myresult:
             member_id, member_username, member_account, member_password = row
+            # Store sign-in status and credentials into sessionmiddleware
+            request.session.update({
+                "SIGNED_IN": True,
+                "MEMBER_ID": member_id,
+                "MEMBER_USERNAME": member_username,
+                "MEMBER_ACCOUNT": member_account,
+                "MEMBER_PASSWORD": member_password
+            })
+        return RedirectResponse(url="/member", status_code=303)
     
     
 
@@ -100,12 +109,44 @@ async def error(request: Request, message: str):  # message for query parameter
 @app.get("/member", response_class=HTMLResponse)
 async def success(request: Request):
     # block any access without green flag
-    if not request.session["SIGNED-IN"] == True:
-        return RedirectResponse(url="/", status_code=302)
+    if not request.session["SIGNED_IN"] == True:
+        return RedirectResponse(url="/?message=請重新登入", status_code=303)
     else:
+        #access database and message table
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=DBpassword,
+            database="website"
+        )
+        mycursor = mydb.cursor()
+        # select all message
+        sql = "SELECT \
+            message.id AS message,\
+            member.username AS member, \
+            message.context AS message \
+            FROM member \
+            INNER JOIN message ON member.id = message.member_id"
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
+        # create structure of template literate
+        message = ""
+        for m in myresult:
+            message += f'<form id="delete-form-{m[0]}" style="display: inline" onsubmit="deleteMessage(event)"> \
+                <b>{m[1]}</b> : {m[2]} \
+                <input type="hidden" name="message_id" value="{m[0]}">'
+            if m[1] == request.session["MEMBER_USERNAME"]:
+                message += '<input type="submit" value="x">\
+                </form>'
+            else:
+                message += '</form>'
+        # return response
         return templates.TemplateResponse(
             request=request,
             name='member.html',
+            context={
+                "username": request.session["MEMBER_USERNAME"],
+                "message":message},
             status_code=200
         )
 
@@ -113,8 +154,50 @@ async def success(request: Request):
 @app.get("/signout", response_class=HTMLResponse)
 async def logout(request: Request):
     # switch the flag back to red
-    request.session.update({"SIGNED-IN": False})
-    return RedirectResponse(url="/", status_code=302)
+    request.session.update({"SIGNED_IN": False})
+    return RedirectResponse(url="/?message=已成功登出", status_code=302)
+
+
+@app.post("/createMessage", response_class=HTMLResponse)
+async def createMessage(request: Request, message: str = Form(None)):
+    message = message.strip()
+    if not request.session["SIGNED_IN"] == True:
+        return RedirectResponse(url="/?message=登入已過時，請重新登入再嘗試", status_code=303)
+    else:
+        # connect to MySQL 'website' database
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=DBpassword,
+            database="website"
+        )
+        mycursor = mydb.cursor()
+        # INSERT INTO
+        sql = 'INSERT INTO message (member_id, context) VALUES (%s, %s)'
+        val = (request.session["MEMBER_ID"], message)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        # redirect to member page
+        return RedirectResponse(url="/member", status_code=303)
+
+@app.post("/deleteMessage", response_class=HTMLResponse)
+async def deleteMessage(request: Request, message_id: str = Form(None)):
+    if not request.session["SIGNED_IN"] == True:
+        return RedirectResponse(url="/?message=登入已過時，請重新登入再嘗試", status_code=303)
+    else:
+        # connect to MySQL 'website' database
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=DBpassword,
+            database="website"
+        )
+        mycursor = mydb.cursor()
+        # Delete message by ID
+        sql = f"DELETE FROM message WHERE id = '{message_id}'"
+        mycursor.execute(sql)
+        mydb.commit()
+        return RedirectResponse(url="/member", status_code=303)
 
 if __name__ == "__main__":
    uvicorn.run("main:app",host = "127.0.0.1", port= 8000, log_level="info")
